10ea268548a047e07e55b38397eff389a5dfd92c
Steffen Seckler <seckler@in.tum.de>
2021-04-27 13:20:37 +0200

    some barriers

--- main.cpp
+++ main.cpp
@@ -16,23 +16,26 @@ int main(int argc, char **argv) {
 
 	int error_flag = 0;
 	std::cout << "Starting ping pong" << std::endl;
+	MPI_Barrier(MPI_COMM_WORLD);
 	auto start = std::chrono::high_resolution_clock::now();
 	if (rank == 0) {
-		int i = 0;
-
+		const int i = 0;
+		int other;
 		MPI_Send(&i, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-		MPI_Recv(&i, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		if (i != 1) {
+		MPI_Recv(&other, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		if (other != 1) {
 			error_flag = 1;
 		}
 	} else {
-		int i = 1;
+		const int i = 1;
+		int other;
+		MPI_Recv(&other, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		MPI_Send(&i, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		MPI_Recv(&i, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		if (i != 0) {
+		if (other != 0) {
 			error_flag = 1;
 		}
 	}
+	MPI_Barrier(MPI_COMM_WORLD);
 	auto end = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 	MPI_Finalize();
